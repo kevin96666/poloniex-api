37ebc29e9fa7606b6987acf4cf1b05861aa22c8a
kevin codfert <kevin.codfert@gmail.com>
2017-07-07 12:49:57 +0200

    Update Request.php
    Fix mem leak by closing curl connection

--- src/tools/Request.php
+++ src/tools/Request.php
@@ -85,9 +85,11 @@ class Request {
         // run the query
         $res = curl_exec(self::$ch);
         if ($res === false) {
-            throw new \Exception("Curl error: " . curl_error(self::$ch));
+            $e = curl_error(self::$ch);
+            curl_close(self::$ch);
+            throw new \Exception("Curl error: " . curl_error($e));
         }
-
+        curl_close(self::$ch);
         $json = json_decode($res, true);
 
         // Check for the Poloniex API error
